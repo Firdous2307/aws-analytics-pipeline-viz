ceac9a57e2e246ce9c83983443338e4147641a07
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-12-27 16:57:21 +0000

    update README and crawler script

--- aws-glue/crawler_script.sh
+++ aws-glue/crawler_script.sh
@@ -1,6 +1,8 @@
 #aws glue create-crawler \
 #    --name "MyCrawler" \
-#    --role "arn:aws:iam::$ACCOUNT_ID:role/service-role/AWSGlueServiceRole-demo" \
+#    --role "arn:aws:iam::$ACCOUNT_ID:role/service-role/AWSGlueServiceRole-Demo" \
 #    --database-name "my-demo-database" \
-#    --description "Crawler for my data" \
-#    --targets "S3Targets=[{Path='$PATH_TO_MY_S3_BUCKET'}]"
+#    --description "Crawler for my dataset" \
+#    --targets "S3Targets=[{Path='firdous-data-analytics-bucket/'}]"
+
+
