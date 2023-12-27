0e79dc658405160b8915e88a84eec6545ca8420a
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-12-27 17:15:30 +0000

    crawler now ready and running

--- aws-glue/crawler_script.sh
+++ aws-glue/crawler_script.sh
@@ -1,8 +1,14 @@
-#aws glue create-crawler \
-#    --name "MyCrawler" \
-#    --role "arn:aws:iam::$ACCOUNT_ID:role/service-role/AWSGlueServiceRole-Demo" \
-#    --database-name "my-demo-database" \
-#    --description "Crawler for my dataset" \
-#    --targets "S3Targets=[{Path='firdous-data-analytics-bucket/'}]"
+# Create Glue Crawler
+aws glue create-crawler \
+    --name "MyCrawler" \
+    --role "arn:aws:iam::$ACCOUNT_ID:role/service-role/AWSGlueServiceRole-Demo" \
+    --database-name "my-demo-database" \
+    --description "Crawler for my dataset" \
+    --targets "S3Targets=[{Path='firdous-data-analytics-bucket/'}]"
 
+# Sleep for 1-2 minutes (choose the appropriate duration)
+echo "Waiting for 1-2 minutes before starting the crawler..."
+sleep 120  # 120 seconds = 2 minutes
 
+# Start the Glue Crawler
+aws glue start-crawler --name MyCrawler
