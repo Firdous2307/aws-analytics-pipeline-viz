da6ba91eee7ae5c77f21e7801caee2bd96fc6ade
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2023-12-29 17:58:20 +0000

    updated script for aws athena

--- aws-athena/script.sh
+++ aws-athena/script.sh
@@ -1,3 +1,9 @@
+# Initiating AWS Athena query execution to retrieve all data from a specific table
 aws athena start-query-execution \
-    --query-string "file://sample_query.sql" \
+    --query-string "SELECT * FROM my-demo-database.firdous_data_analytics_bucket" \
     --result-configuration "OutputLocation=s3://firdous-data-analytics-bucket/"
+
+# Initiating AWS Athena query execution using an SQL file
+aws athena start-query-execution \
+    --query-string aws-athena/athena_query.sql \
+    --result-configuration OutputLocation=s3://firdous-data-analytics-bucket/
