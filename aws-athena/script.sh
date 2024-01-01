825871f20c7f58876131a6c63c89b5c76ad37ba1
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2024-01-01 07:08:29 +0000

    updated athena script

--- aws-athena/script.sh
+++ aws-athena/script.sh
@@ -1,9 +1,11 @@
 # Initiating AWS Athena query execution to retrieve all data from a specific table
-aws athena start-query-execution \
-    --query-string "SELECT * FROM my-demo-database.firdous_data_analytics_bucket" \
-    --result-configuration "OutputLocation=s3://firdous-data-analytics-bucket/"
+    aws athena start-query-execution \
+    --query-string "SELECT * FROM \"my-demo-database\".\"firdous_data_analytics_bucket\" LIMIT 10" \
+    --result-config "OutputLocation=s3://firdous-data-analytics-bucket/" \
+    --query-execution-context Database="my-demo-database",Catalog="AwsDataCatalog"
 
+    
 # Initiating AWS Athena query execution using an SQL file
 aws athena start-query-execution \
-    --query-string aws-athena/athena_query.sql \
-    --result-configuration OutputLocation=s3://firdous-data-analytics-bucket/
+    --query-string "file://sample_query.sql" \
+    --result-configuration "OutputLocation=s3://firdous-data-analytics-bucket/"
