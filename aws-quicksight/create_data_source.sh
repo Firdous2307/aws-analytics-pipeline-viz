c83d82ff9c9750399a35d81d9f1ceed2d287ce3f
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2024-01-02 12:24:00 +0000

    work in progress

--- aws-quicksight/create_data_source.sh
+++ aws-quicksight/create_data_source.sh
@@ -1,10 +1,14 @@
-#aws quicksight create-data-source \
-#    --aws-account-id "$ACCOUNT_ID" \
-#    --name "MyAthenaDataSource" \
-#    --data-source-id "AthenaDataSource-2024" \
-#    --type ATHENA \
-#    --data-source-parameters "AthenaParameters={WorkGroup=primary}" \
-#    --region us-east-2
-
-
-
+aws quicksight create-data-source \
+    --aws-account-id "$ACCOUNT_ID" \
+    --name "MyS3DataSource" \
+    --data-source-id "S3DataSource-2024" \
+    --type S3 \
+    --data-source-parameters '{
+        "S3Parameters": {
+            "ManifestFileLocation": {
+                "Bucket": "firdous-data-analytics-bucket",
+                "Key": "manifest.json"
+            }
+        }
+    }' \
+    --region us-east-2
